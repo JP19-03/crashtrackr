f1615107afe67fdf8ae257a65b91646a6c8cd5a2
JP19-03 <u202014511@upc.edu.pe>
2025-01-23 20:46:37 -0500

    feat: add database connection using Sequelize and dotenv.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,19 @@
 import express from 'express'
 import colors from 'colors'
 import morgan from 'morgan'
+import { db } from './config/db'
+
+async function connectDB() {
+    try {
+        await db.authenticate()
+        db.sync()
+        console.log(colors.blue.bold('Successfully connected to the database'))
+    } catch (error) {
+        //console.error(error)
+        console.log(colors.red.bold('Failed to connect to the database'))
+    }
+}
+connectDB()
 
 const app = express()
 
