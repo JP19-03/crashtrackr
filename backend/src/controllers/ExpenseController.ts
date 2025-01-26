99ae43c44c67a004ce5e9a6e56f1205dc5493f1e
JP19-03 <u202014511@upc.edu.pe>
2025-01-25 21:41:39 -0500

    feat: implement ExpensesController for managing expenses and link to Budget model

---
+++ backend/src/controllers/ExpenseController.ts
@@ -0,0 +1,30 @@
+import type { Request, Response } from 'express'
+import Expense from '../models/Expense';
+
+export class ExpensesController {
+    static create = async (req: Request, res: Response) => {
+        try {
+            const expense = new Expense(req.body);
+            expense.budgetId = req.budget.id;
+            await expense.save();
+            res.status(201).json("Expense created successfully");
+        } catch (error) {
+            // console.log(error);
+            res.status(500).json({ message: "An error occurred while creating the expense" });
+        }
+    }
+
+    static getById = async (req: Request, res: Response) => {
+        res.json(req.expense);
+    }
+
+    static updateById = async (req: Request, res: Response) => {
+        await req.expense.update(req.body);
+        res.json("Expense updated successfully");
+    }
+
+    static deleteById = async (req: Request, res: Response) => {
+        await req.expense.destroy();
+        res.json("Expense deleted successfully");
+    }
+}
