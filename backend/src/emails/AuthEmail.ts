d40c81574e6177c10cc9bfb2c2a1f55121944e61
JP19-03 <u202014511@upc.edu.pe>
2025-01-29 12:36:55 -0500

    feat: update email subjects and add password reset email functionality

--- backend/src/emails/AuthEmail.ts
+++ backend/src/emails/AuthEmail.ts
@@ -11,11 +11,26 @@ export class AuthEmail {
         const email = await transport.sendMail({
             from: 'CashTrackr <admin@cashtrackr.com>',
             to: user.email,
-            subject: 'CashTrackr Account Confirmation',
+            subject: 'CashTrackr - Account Confirmation',
             html: `
                 <h1>Hi ${user.name}</h1>
                 <p>Thank you for creating an account with CashTrackr. Please click the link below to confirm your account.</p>
                 <a href="http://localhost:3000/confirm/${user.token}">Confirm Account</a>
+                <p>And enter the following token: ${user.token}</p>
+            `
+        })
+        console.log("Message sent: %s", email.messageId)
+    }
+
+    static sendPasswordResetToken = async (user: EmailType) => {
+        const email = await transport.sendMail({
+            from: 'CashTrackr <admin@cashtrackr.com>',
+            to: user.email,
+            subject: 'CashTrackr - Password Reset',
+            html: `
+                <h1>Hi ${user.name}</h1>
+                <p>You have requested to reset your password. Please click the link below to reset your password.</p>
+                <a href="http://localhost:3000/reset-password/${user.token}">Reset Password</a>
                 <p>And enter the following token: ${user.token}</p>
             `
         })
